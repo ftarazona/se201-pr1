b519cce661ef8c159653536c2581a4000b2c5c6f
Florian Tarazona <florian.tarazona@telecom-paris.fr>
2021-12-23 22:29:35 +0100

    Corrected add_vector.c

--- part2/add_vector.c
+++ part2/add_vector.c
@@ -1,6 +1,6 @@
 int add_vector(int *v1, int *v2, int *vtot, int size)	{
 	if(!v1 || !v2 || !vtot)	{ return -1; }
-	for(int i = 0; i < size++; i++)
+	for(int i = 0; i < size; i++)
 		vtot[i] = v1[i] + v2[i];
 	return size;
 }
